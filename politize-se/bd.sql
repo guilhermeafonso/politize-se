74aee79feaff73de03094c755655a7030b859f97
guilhermeafonso <afonsoguilherme.93@hotmail.com>
2017-10-04 00:06:05 -0300

    Desenvolcimento do projeto

---
+++ politize-se/bd.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04-Out-2017 às 00:55
+-- Versão do servidor: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bd_politizese`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bd`
+--
+
+CREATE TABLE `bd` (
+  `Nome` text NOT NULL,
+  `Idade` int(2) NOT NULL,
+  `Sexo` varchar(10) NOT NULL,
+  `Cor/raça` varchar(10) NOT NULL,
+  `Espectro econômico` varchar(50) NOT NULL,
+  `Espectro social` varchar(50) NOT NULL,
+  `Tipo de governo` varchar(50) NOT NULL,
+  `Sistema pol. econ.` varchar(50) NOT NULL,
+  `Religião e política 1` varchar(50) NOT NULL,
+  `Religião e política 2` varchar(50) NOT NULL,
+  `Sistema eleitoral` varchar(50) NOT NULL,
+  `Voto eleitoral` varchar(50) NOT NULL,
+  `Meu partido` varchar(50) NOT NULL,
+  `Meu candidato` varchar(50) NOT NULL,
+  `Simulação de voto` varchar(50) NOT NULL
+ 
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ politize-se/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<script src="js/jquery-3.2.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<title>Politize-se!</title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="logo1" alt="Logo" src="imagens/titulo.png">
+						<div class="caixainicio">
+							<p>O projeto <b><i>politize-se</i></b> tem como objetivo apresentar de forma didática informações, definições e questionamentos sobre política, com o intuito principal de desenvolver sua ideologia, apresentar um pouco mais sobre o assunto e criar seu perfil eleitoral.</p>
+							<p>É rápido, fácil e o mais importante: pode te preparar melhor para as próximas eleições!</p>
+						</div>
+						<a href="conteudo/cadastro.php" class="botao1" role="button"><b>Começar</b></a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+	      	<div class="rodape">
+	      		<div class="container">      			
+	      			<b>Copyright© 2017</b>
+	      		</div>
+	     	</div>
+	    </section>
+	</body>
+</html>
